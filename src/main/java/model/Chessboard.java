61d06d9928d6cee40c1dd1e6eb3a7ef7435e8a33
Rodina3 <buaazhourong@163.com>
2018-06-11 23:09:51 +0800

    [Rong] Refactor Chessboard

--- src/main/java/model/Chessboard.java
+++ src/main/java/model/Chessboard.java
@@ -16,31 +16,67 @@ public class Chessboard {
     }
 
     public int getAliveNeighborNumber(Position position) {
-        int leftBoundary = position.getX() - 1 == 0 ? 1 : position.getX() - 1;
-        int rightBoundary = position.getX() + 1 > length ? length : position.getX() + 1;
-        int upperBoundary = position.getY() - 1 == 0 ? 1 : position.getY() - 1;
-        int bottomBoundary = position.getY() + 1 > width ? width : position.getY() + 1;
+        int leftBoundary = getLeftBoundary(position);
+        int rightBoundary = getRightBoundary(position);
+        int upperBoundary = getUpperBoundary(position);
+        int bottomBoundary = getBottomBoundary(position);
 
-        int aliveNeighborCount = 0;
-        for (int i = leftBoundary; i <= rightBoundary; i++) {
-            for (int j = upperBoundary; j <= bottomBoundary; j++) {
-                if (cells.get(new Position(i, j)).getCellStatus() == CellStatus.ALIVE) {
-                    aliveNeighborCount++;
-                }
-            }
-        }
-        return cells.get(position).getCellStatus() == CellStatus.ALIVE ? aliveNeighborCount - 1 : aliveNeighborCount;
+        int aliveNumbers = countAliveNumbers(leftBoundary, rightBoundary, upperBoundary, bottomBoundary);
+        return minusCellItself(position, aliveNumbers);
     }
 
     public Chessboard getNextChessBoard() {
         for (int i = 1; i <= length; i++) {
             for (int j = 1; j <= width; j++) {
                 Position position = new Position(i, j);
-                Cell cell = this.cells.get(position);
-                cells.put(position, new Cell(cell.getNextStatus(getAliveNeighborNumber(position))));
+                Cell oldCell = getCell(position);
+                Cell newCell = new Cell(oldCell.getNextStatus(getAliveNeighborNumber(position)));
+                cells.put(position, newCell);
             }
         }
         return this;
+    }
+
+    private Cell getCell(Position position) {
+        return this.cells.get(position);
+    }
+
+    private int getBottomBoundary(Position position) {
+        return position.getY() + 1 > width ? width : position.getY() + 1;
+    }
+
+    private int getUpperBoundary(Position position) {
+        return position.getY() - 1 == 0 ? 1 : position.getY() - 1;
+    }
+
+    private int getRightBoundary(Position position) {
+        return position.getX() + 1 > length ? length : position.getX() + 1;
+    }
+
+    private int getLeftBoundary(Position position) {
+        return position.getX() - 1 == 0 ? 1 : position.getX() - 1;
+    }
+
+    private int minusCellItself(Position position, int aliveNumbers) {
+        return getCell(position).getCellStatus() == CellStatus.ALIVE ? aliveNumbers - 1 : aliveNumbers;
+    }
+
+    private int countAliveNumbers(int leftBoundary, int rightBoundary, int upperBoundary, int bottomBoundary) {
+        int aliveNumbers = 0;
+        for (int i = leftBoundary; i <= rightBoundary; i++) {
+            aliveNumbers = aliveNumbers + countEachRow(upperBoundary, bottomBoundary, i);
+        }
+        return aliveNumbers;
+    }
+
+    private int countEachRow(int upperBoundary, int bottomBoundary, int i) {
+        int aliveNumbers = 0;
+        for (int j = upperBoundary; j <= bottomBoundary; j++) {
+            if (getCell(new Position(i, j)).getCellStatus() == CellStatus.ALIVE) {
+                aliveNumbers++;
+            }
+        }
+        return aliveNumbers;
     }
 
     public Map<Position, Cell> getCells() {
